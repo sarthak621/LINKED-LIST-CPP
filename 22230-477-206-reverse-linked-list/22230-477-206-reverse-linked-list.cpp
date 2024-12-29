bbf8d0f61a48a70decf0ed6a20050db07af04083
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2024-12-29 19:09:03 +0530

    Time: 0 ms, Memory: 13.4 MB - LeetHub

--- 22230-477-206-reverse-linked-list/22230-477-206-reverse-linked-list.cpp
+++ 22230-477-206-reverse-linked-list/22230-477-206-reverse-linked-list.cpp
@@ -10,19 +10,42 @@
  */
 class Solution {
 public:
-    ListNode* reverseList(ListNode* head) {
-            //reversing the LL 
-            // *************ITERATIVE APPROACH*****************
 
+    ListNode* reverseLL(ListNode* prev,ListNode* curr ){
+        // base case
+        if(curr==NULL){
+            return prev;
+        }
+
+        // 1 case
+        ListNode* nextNode=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=nextNode;
+
+        //baki recursion
+         return reverseLL(prev,curr);
+
+    }
+
+    ListNode* reverseList(ListNode* head) {
             ListNode* prev=NULL;
             ListNode* curr=head;
-
-            while(curr!=NULL){
-                ListNode* nextNode=curr->next;
-                curr->next=prev;
-                prev=curr;
-                curr=nextNode;
-            }       
-            return prev;
+         return reverseLL(prev,curr);
     }
 };
+
+
+//    //reversing the LL 
+//             // *************ITERATIVE APPROACH*****************
+
+//             ListNode* prev=NULL;
+//             ListNode* curr=head;
+
+//             while(curr!=NULL){
+//                 ListNode* nextNode=curr->next;
+//                 curr->next=prev;
+//                 prev=curr;
+//                 curr=nextNode;
+//             }       
+//             return prev;

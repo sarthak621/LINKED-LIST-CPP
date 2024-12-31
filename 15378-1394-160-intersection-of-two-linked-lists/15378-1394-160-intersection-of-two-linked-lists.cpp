496947b4ea73932507e5798a2980e6ba3198486b
Sarthak Srivastav <srivastavsarthak6211@gmail.com>
2024-12-31 22:37:00 +0530

    Time: 43 ms, Memory: 18.6 MB - LeetHub

--- 15378-1394-160-intersection-of-two-linked-lists/15378-1394-160-intersection-of-two-linked-lists.cpp
+++ 15378-1394-160-intersection-of-two-linked-lists/15378-1394-160-intersection-of-two-linked-lists.cpp
@@ -8,53 +8,23 @@
  */
 class Solution {
 public:
-
-    ListNode* findCollision(ListNode* small,ListNode* high,int diff){
-            while(diff){
-                diff--;
-                high=high->next;
-            }
-
-            while(small!=high){
-                small=small->next;
-                high=high->next;
-            }
-            return small;
-    }
-
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        
-        if(headA==NULL||headB==NULL) return NULL;
-        
-        //approach of length diffrence
-        ListNode* head1=headA;
-        ListNode* head2=headB;
-        
-        //calculating the length of both t1,t2
-        int t1=0;
-        int t2=0;
-        while(head1!=NULL){
-           t1++;
-           head1=head1->next; 
+        if(headA==NULL || headB==NULL) return NULL;
+
+        ListNode* t1=headA;
+        ListNode* t2=headB;
+
+        while(t1!=t2){
+            t1=t1->next;
+            t2=t2->next;
+
+            if(t1==t2) return t1;
+
+            if(t1==NULL)  t1=headB; 
+            if(t2==NULL) t2=headA;
         }
-
-        while(head2!=NULL){
-           t2++;
-           head2=head2->next; 
-        }   
-
-        head1=headA;
-        head2=headB;    
-
-        if(t2>t1){
-            return findCollision(head1,head2,(t2-t1));
-        } 
-
-        else{
-            return findCollision(head2,head1,(t1-t2));
-        }
-
+        return t1;
     }
 };
 
-//T.C -->O(n1)+O(n2)+O(n2-n1)+O(n1)
+//O(n1+n2)   t.c.
